2ae9b69c8ff5cc7d0f9cf4f461710c4aca75e206
Mark Chweya <chweyamark@gmail.com>
2026-03-11 06:43:48 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './Login'
+import Login from '../Login'
 
 function StudentDashboard(){
   return <h1>Student Dashboard</h1>
