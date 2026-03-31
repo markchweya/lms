51ee5b8e6168c7633032aafce96d9ed855c4867a
Mark Chweya <chweyamark@gmail.com>
2026-03-31 14:03:01 +0300

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import AdminDashboard from './admin/AdminDashboard';
+import Login from './Login';
 
 function App() {
-  return <AdminDashboard />;
+  return <Login />;
 }
 
 export default App;
